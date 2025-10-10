c61ad4024ef50304aed74a2292c44e920dc1eb06
jin <ekdekdgkrp20@gmail.com>
2025-10-10 18:22:26 +0900

    🐛 fix: resolve MIME type issue for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// 깃허브 페이지 주소가 panicathe.github.io 인 경우 base는 '/' 로 유지
 export default defineConfig({
-  assetsInclude: ['**/*.glb'],
+  base: '/',
   plugins: [react()],
+  assetsInclude: ['**/*.glb'],
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+    },
+  },
 })
