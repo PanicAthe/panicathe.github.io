94f95c12d3542187fb8168a2e9f1e0de9dcc526f
jin <ekdekdgkrp20@gmail.com>
2025-10-10 20:48:27 +0900

    🚀 update portfolio site

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  root: '.', // ✅ 명시적으로 지정
+  base: '/', // ✅ GitHub Pages 루트니까 '/' 유지
+  publicDir: 'public', // ✅ 명시
   plugins: [react()],
-  assetsInclude: ['**/*.glb', '**/*.mp4'],
+  assetsInclude: ['**/*.mp4', '**/*.glb', '**/*.webm'], // ✅ mp4 추가
   build: {
+    outDir: 'dist',
     rollupOptions: {
       input: 'index.html',
     },
-    copyPublicDir: true,
+    copyPublicDir: true, // ✅ 반드시 포함
   },
 })
