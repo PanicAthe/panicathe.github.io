7e074df4b37263efc6035cfd76b8c4f535db0401
jin <ekdekdgkrp20@gmail.com>
2025-10-10 20:33:25 +0900

    🚀 update portfolio site

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 깃허브 페이지 주소가 panicathe.github.io 인 경우 base는 '/' 로 유지
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  assetsInclude: ['**/*.glb'],
+  assetsInclude: ['**/*.glb', '**/*.mp4'], 
   build: {
     rollupOptions: {
       input: 'index.html',
